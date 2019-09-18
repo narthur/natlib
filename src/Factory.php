654469265a01984f6389af1c8e6f02273390fb78
Nathan Arthur <nathan@nathanarthur.com>
2019-09-18 10:50:33 -0400

    Reformat

--- src/Factory.php
+++ src/Factory.php
@@ -6,70 +6,70 @@ use ReflectionException;
 
 class Factory
 {
-	private $objects = [];
+    private $objects = [];
 
-	public function __construct(...$objects)
-	{
-		$this->injectObjects($this, ...$objects);
-	}
+    public function __construct(...$objects)
+    {
+        $this->injectObjects($this, ...$objects);
+    }
 
-	public function injectObjects(...$objects)
-	{
-		array_map([$this, 'cache'], $objects);
-	}
+    public function injectObjects(...$objects)
+    {
+        array_map([$this, 'cache'], $objects);
+    }
 
-	/**
-	 * @param $class
-	 * @return mixed
-	 * @throws ReflectionException
-	 */
-	public function secure($class)
-	{
-		return $this->getSavedObject($class)
+    /**
+     * @param $class
+     * @return mixed
+     * @throws ReflectionException
+     */
+    public function secure($class)
+    {
+        return $this->getSavedObject($class)
             ?: $this->cache($this->make($class), $class);
-	}
+    }
 
-	/**
-	 * @param $class
-	 * @return mixed
-	 * @throws ReflectionException
-	 */
-	public function make($class)
-	{
-		$dependencies = $this->getDependencies($class);
+    /**
+     * @param $class
+     * @return mixed
+     * @throws ReflectionException
+     */
+    public function make($class)
+    {
+        $dependencies = $this->getDependencies($class);
 
-		return new $class(...$dependencies);
-	}
+        return new $class(...$dependencies);
+    }
 
-	/**
-	 * @param $class
-	 * @return array
-	 * @throws ReflectionException
-	 */
-	private function getDependencies($class)
-	{
-		$dependencyNames = $this->getDependencyNames($class);
+    /**
+     * @param $class
+     * @return array
+     * @throws ReflectionException
+     */
+    private function getDependencies($class)
+    {
+        $dependencyNames = $this->getDependencyNames($class);
 
-		return array_map([$this, "secure"], $dependencyNames);
-	}
+        return array_map([$this, "secure"], $dependencyNames);
+    }
 
-	/**
-	 * @param $className
-	 * @return array|mixed
-	 * @throws ReflectionException
-	 */
-	private function getDependencyNames($className)
-	{
-		$reflection = new \ReflectionClass($className);
-		$constructor = $reflection->getConstructor();
-		$params = ($constructor) ? $constructor->getParameters() : [];
+    /**
+     * @param $className
+     * @return array|mixed
+     * @throws ReflectionException
+     */
+    private function getDependencyNames($className)
+    {
+        $reflection = new \ReflectionClass($className);
+        $constructor = $reflection->getConstructor();
+        $params = ($constructor) ? $constructor->getParameters() : [];
 
-		return array_map(function (\ReflectionParameter $param) {
-			return $param->getClass()->getName();
-		}, $params);
-	}
+        return array_map(function (\ReflectionParameter $param) {
+            return $param->getClass()->getName();
+        }, $params);
+    }
 
-	/**
+    /**
      * Aggressive caching allows us to avoid expensive identity-based lookups
      */
     private function cache($object, $class = null)
@@ -84,12 +84,12 @@ class Factory
         return $this->objects[$class] = $object;
     }
 
-	/**
-	 * @param $class
-	 * @return mixed
-	 */
-	private function getSavedObject($class)
-	{
-	    return $this->objects[$class] ?? null;
-	}
+    /**
+     * @param $class
+     * @return mixed
+     */
+    private function getSavedObject($class)
+    {
+        return $this->objects[$class] ?? null;
+    }
 }
